f51cc04d325ffd2a282111f56c66849aa78df9df
tfurr01 <tfurr01@gmail.com>
2021-04-19 14:39:36 -0400

    create operate function

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+
+function operate(num1, num2, op) {
+    if (op === 'plus') {
+        add(num1, num2);
+    }
+    else if (op === 'minus') {
+        subtract(num1, num2);
+    }
+    else if (op === 'multiply') {
+        multiply(num1, num2);
+    }
+    else {
+        divide(num1, num2);
+    }
+}
+
